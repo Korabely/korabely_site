1b6aa2aa93a81e02785d22cccae93a5a84b50aee
Korabely <korabely.mykola@gmail.com>
2025-09-05 23:39:42 +0300

    Delete config.telegram.php

--- config.telegram.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-  'BOT_TOKEN' => '8460659571:AAEn6pqDb1RAHOGYa7gTqYBhbbi5wGs0jPM',
-  'CHAT_ID'   => '@korabely_media',
-  'DEFAULT_PARSE_MODE' => 'HTML', // або '', або 'MarkdownV2'
-];
